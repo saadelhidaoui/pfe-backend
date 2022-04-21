efdcb677b3e2d39e3a77cf760878eb8428b67d2c
Saad Elhidaoui <saadelhidaoui@gmail.com>
2022-04-21 11:30:49 +0000

    création du projet

---
+++ src/main/java/com/example/pfebackend/PfeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfeBackendApplication.class, args);
+    }
+
+}
